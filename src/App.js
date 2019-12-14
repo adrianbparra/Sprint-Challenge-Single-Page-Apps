25388622d32f73b5f76abb4e025fe2f18ca98b09
adrianbparra <adrianplane12@gmail.com>
2019-12-14 10:37:49 -0700

    added all the characters to display in page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./components/Header.js";
+import CharacterList from "./components/CharacterList.js";
+import {Link, Route} from "react-router-dom";
 
 
 export default function App() {
   return (
     <main>
-      <Header />
+      <Route path="/" component={Header} />
+      <Route path="/" component={CharacterList} />
     </main>
   );
 }
--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,10 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard({character}) {
+  return (
+    <div key={character.id}>
+      <img src={character.image} alt={character.name}/>
+      <h2>{character.name}</h2>
+    </div>
+  );
 }
